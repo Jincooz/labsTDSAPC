0b735d1e07abf764fa652a44365e752d52908ab4
Vitalii <shaddyreaper@gmail.com>
2022-11-30 22:47:51 +0200

    test main_proc

--- hw27.10/main_proc.py
+++ hw27.10/main_proc.py
@@ -1,6 +1,7 @@
 from mpi4py import MPI
 import random
 from Point import *
+import time
 
 def get_amount_of_dots():
     while True:
@@ -21,7 +22,14 @@ def start(comm):
         send_amount_of_dots = int(amount_of_dots/(comm_size - 1) * procid)
         message = dots[0:send_amount_of_dots]
         comm.send(message, dest = procid)
+    start = time.time()
     for procid in range(1, comm_size):
+        f1  = time.time()
         pi = comm.recv(source = procid)
+        f2 = time.time()
         ctime = comm.recv(source = procid)
+        f3 = time.time()
         print(str(procid) + " programm end his work. His pi = " + str(pi) + ". Time = " + str(ctime) + ".")
+        print(f"{f3-f2}s {f2-f1}s {f3-f1}s")
+    end = time.time()
+    print(f"I wait {end - start}s")
